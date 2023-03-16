f49d9e74a5bbd1692fb985dd36e6c2f06f87e3e0
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-03-17 01:00:36 +0300

    feat: add database, pages, components folders

---
+++ react-app/src/models.ts
@@ -0,0 +1,11 @@
+export interface ICarSchema {
+  id: number;
+  make: string;
+  model: string;
+  price: number;
+  age: number;
+  fuel: string;
+  miles: string;
+  city: string;
+  img: string;
+}
