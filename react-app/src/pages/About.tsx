e7d417463f6e0cdcbe9158f881d041a1c3f813c4
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-03-20 13:55:27 +0300

    feat: add info about us

--- react-app/src/pages/About.tsx
+++ react-app/src/pages/About.tsx
@@ -10,7 +10,13 @@ export default class About extends Component {
       <>
       <Navigation />
       <div className='flex my-40 items-center justify-center'>
-        <h1 className='m-20'>The project made by <span className='font-bold'>Kirill Kolchanov</span></h1>
+        <h1 className='m-20'>
+          The project made by
+          <span className='font-bold'> Kirill Kolchanov</span>
+          <p className='mt-4 mb-4'>My discord: <span className='italic'>KirillKolchanov#2036</span></p>
+          <a href="https://github.com/KirillKolchanov/">My Github</a>
+          <div className='mt-4'><a href="https://t.me/kirya_ko">My Telegram</a></div>
+        </h1>
         <div className='imgAbout bg-cover items-center rounded-full' />
       </div>
       </>
