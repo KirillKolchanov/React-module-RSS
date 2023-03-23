72a1ea0ac3e6ac8c71f15dad92b6d5301699ddd3
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-03-24 00:08:12 +0100

    feat: add select input in Forms page

--- react-app/src/components/Forms.tsx
+++ react-app/src/components/Forms.tsx
@@ -7,25 +7,33 @@ import { ICarSchema } from '../models';
 
 export default class Forms extends Component {
   carMakeRef = createRef<HTMLInputElement>();
+  carModelRef = createRef<HTMLInputElement>();
 
   constructor(props: object) {
     super(props);
 
     this.state = {
       carMake: '',
+      carModel: '',
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.onChange = this.onChange.bind(this);
+    this.onMakeChange = this.onMakeChange.bind(this);
+    this.onModelChange = this.onModelChange.bind(this);
   }
 
   handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
   }
 
-  onChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+  onMakeChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
     const carMake = this.carMakeRef.current?.value;
     this.setState({ carMake });
+  }
+
+  onModelChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    const carModel = this.carModelRef.current?.value;
+    this.setState({ carModel });
   }
 
   render(): JSX.Element {
@@ -34,19 +42,66 @@ export default class Forms extends Component {
         {/* <img src="../../public/cars/forms_background.png" alt="" /> */}
         <div className="form-floating mt-3 xl:w-96">
           <form className="flex flex-col" onSubmit={this.handleSubmit}>
-            <h2 className="text-center mb-5 font-bold">Car constructor</h2>
+            <h2 className="text-center mb-10 font-bold">Car constructor</h2>
             <input
               className="border-2 rounded py-2 px-3 required"
               type="text"
               // className={className}
               placeholder="Car make"
-              name="Car make"
+              name="carMake"
               ref={this.carMakeRef}
-              onChange={this.onChange}
-              // ref={this.props.reference}
+              onChange={this.onMakeChange}
+              data-testid="textInput"
+              // id={this.props.subject}
+              required
+            />
+            <input
+              className="mt-10 border-2 rounded py-2 px-3 required"
+              type="text"
+              // className={className}
+              placeholder="Car model"
+              name="carModel"
+              ref={this.carModelRef}
+              onChange={this.onModelChange}
               data-testid="textInput"
               // id={this.props.subject}
             />
+            <select
+              className="mt-10 rounded py-2 px-3 border-2 required"
+              placeholder="Select year:"
+            >
+              {/* // valid={this.state.gender}
+              // subject="Select gender"
+              // name="gender"
+              // options={['Male', 'Female']}
+              // onChange={this.onChangeHandler.bind(this)}
+              // reference={this.genderSelect}
+              // warningMessage={warningMessages.gender.emptyInput} */}
+              <option value="" disabled selected>
+                Select year:
+              </option>
+              {[
+                '2010',
+                '2011',
+                '2012',
+                '2013',
+                '2014',
+                '2015',
+                '2016',
+                '2017',
+                '2018',
+                '2019',
+                '2020',
+                '2021',
+                '2022',
+              ].map(
+                (year: string): JSX.Element => (
+                  <option key={year} value={year}>
+                    {year}
+                  </option>
+                )
+              )}
+            </select>
             <button
               type="submit"
               className="mt-20 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
