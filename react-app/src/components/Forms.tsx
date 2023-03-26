d8d1b6f0abf774f573a63e91a3c3659766e793a7
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-03-26 01:05:31 +0100

    feat: add submitHandler, checkbox, cardsRender in Forms page

--- react-app/src/components/Forms.tsx
+++ react-app/src/components/Forms.tsx
@@ -1,29 +1,35 @@
-import React, { Component, createRef } from 'react';
-import allCars from '../data/cars';
+import React, { Component, createRef, RefObject } from 'react';
 
 import '../styles/pages/Forms.css';
 
 import { ICarSchema } from '../models';
 import Switcher from './Forms/Switcher';
+import Card from './Card';
 
 type MyProps = Record<string, never>;
 
 type MyState = {
   carMake: string | undefined;
   carModel: string | undefined;
-  carYear: string | undefined;
+  carYear: number | undefined;
   carFuel: string | undefined;
   carPhoto: string | undefined;
+  carMileage: string | undefined;
+  carPrice: number | undefined;
+  allFormCars: ICarSchema[];
 };
 
-export default class Forms extends React.Component<MyProps, MyState> {
-  carMakeRef: React.RefObject<HTMLSelectElement>;
-  carModelRef: React.RefObject<HTMLInputElement>;
-  carYearRef: React.RefObject<HTMLInputElement>;
-  carPetrolRef: React.RefObject<HTMLInputElement>;
-  carDieselRef: React.RefObject<HTMLInputElement>;
-  carElectroRef: React.RefObject<HTMLInputElement>;
-  carPhotoRef: React.RefObject<HTMLInputElement>;
+export default class Forms extends Component<MyProps, MyState> {
+  carMakeRef: RefObject<HTMLSelectElement>;
+  carModelRef: RefObject<HTMLInputElement>;
+  carYearRef: RefObject<HTMLInputElement>;
+  carPetrolRef: RefObject<HTMLInputElement>;
+  carDieselRef: RefObject<HTMLInputElement>;
+  carElectroRef: RefObject<HTMLInputElement>;
+  carPhotoRef: RefObject<HTMLInputElement>;
+  carMileageRef: RefObject<HTMLInputElement>;
+  carPriceRef: RefObject<HTMLInputElement>;
+  checkboxRef: RefObject<HTMLInputElement>;
 
   constructor(props: MyProps) {
     super(props);
@@ -35,13 +41,19 @@ export default class Forms extends React.Component<MyProps, MyState> {
     this.carDieselRef = createRef();
     this.carElectroRef = createRef();
     this.carPhotoRef = createRef();
+    this.carMileageRef = createRef();
+    this.carPriceRef = createRef();
+    this.checkboxRef = createRef();
 
     this.state = {
       carMake: '',
       carModel: '',
-      carYear: '',
+      carYear: 0,
       carFuel: '',
       carPhoto: '',
+      carMileage: '',
+      carPrice: 0,
+      allFormCars: [],
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -52,6 +64,34 @@ export default class Forms extends React.Component<MyProps, MyState> {
     this.onDieselChange = this.onDieselChange.bind(this);
     this.onElectroChange = this.onElectroChange.bind(this);
     this.onPhotoChange = this.onPhotoChange.bind(this);
+    this.onMileageChange = this.onMileageChange.bind(this);
+    this.onPriceChange = this.onPriceChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.clearForm = this.clearForm.bind(this);
+  }
+
+  clearForm() {
+    if (
+      this.carMakeRef.current &&
+      this.carModelRef.current &&
+      this.carPetrolRef.current &&
+      this.carDieselRef.current &&
+      this.carElectroRef.current &&
+      this.carYearRef.current &&
+      this.carPhotoRef.current &&
+      this.carMileageRef.current &&
+      this.carPriceRef.current &&
+      this.checkboxRef.current
+    ) {
+      this.carModelRef.current.value = '';
+      this.carMakeRef.current.value = '';
+      this.carPetrolRef.current.checked = false;
+      this.carYearRef.current.value = '';
+      this.carPhotoRef.current.value = '';
+      this.carMileageRef.current.value = '';
+      this.carPriceRef.current.value = '';
+      this.checkboxRef.current.checked = false;
+    }
   }
 
   handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
@@ -69,7 +109,7 @@ export default class Forms extends React.Component<MyProps, MyState> {
   }
 
   onYearChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
-    const carYear = this.carYearRef.current?.value?.substring(0, 4);
+    const carYear = Number(this.carYearRef.current?.value?.substring(0, 4));
     this.setState({ carYear });
   }
 
@@ -95,136 +135,217 @@ export default class Forms extends React.Component<MyProps, MyState> {
     }
   }
 
+  onMileageChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    const carMileage = this.carMileageRef.current?.value;
+    this.setState({ carMileage });
+  }
+
+  onPriceChange(event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+    const carPrice = Number(this.carPriceRef.current?.value);
+    this.setState({ carPrice });
+  }
+
+  onSubmit(event: React.FormEvent<HTMLFormElement | HTMLSelectElement>) {
+    event.preventDefault();
+    this.clearForm();
+
+    const newArr = [...this.state.allFormCars];
+    newArr.push({
+      make: this.state.carMake ? this.state.carMake : '',
+      model: this.state.carModel ? this.state.carModel : '',
+      price: this.state.carPrice ? this.state.carPrice : 0,
+      age: this.state.carYear ? this.state.carYear : 0,
+      fuel: this.state.carFuel ? this.state.carFuel : '',
+      miles: this.state.carMileage ? this.state.carMileage : '',
+      img: this.state.carPhoto ? this.state.carPhoto : '',
+    });
+    this.setState({
+      allFormCars: newArr,
+    });
+  }
+
   render(): JSX.Element {
     return (
-      <div className="form_wrapper flex pb-72 bg-cover bg-center bg-no-repeat justify-end">
-        <div className="p-10 mt-20 mr-56 form-floating xl:w-1/4 rounded dark:bg-slate-800 bg-white  bg-opacity-75">
-          <form className="flex flex-col" onSubmit={this.handleSubmit}>
-            <h2 className="text-center mb-10 font-bold">Car constructor</h2>
-            <select
-              defaultValue={''}
-              className="mt-10 rounded py-2 px-3 border-2 required"
-              placeholder="Select year:"
-              onChange={this.onMakeChange}
-              ref={this.carMakeRef}
-              required
-            >
-              <option value="" disabled>
-                Select make:
-              </option>
-              {['Aston Martin', 'Audi', 'Bmw', 'Ferrari', 'Mercedes', 'Porsche', 'Volkswagen'].map(
-                (make: string): JSX.Element => (
-                  <option key={make} value={make}>
-                    {make}
-                  </option>
-                )
-              )}
-            </select>
-            {/* <input
-              className="border-2 rounded py-2 px-3"
-              type="text"
-              // className={className}
-              placeholder="Car make"
-              name="carMake"
-              ref={this.carMakeRef}
-              onChange={this.onMakeChange}
-              data-testid="textInput"
-              // id={this.props.subject}
-              required
-            /> */}
-            <input
-              className="mt-10 border-2 rounded py-2 px-3 required"
-              type="text"
-              // className={className}
-              placeholder="Car model"
-              name="carModel"
-              ref={this.carModelRef}
-              onChange={this.onModelChange}
-              data-testid="textInput"
-              // id={this.props.subject}
-              required
-            />
-            <div className="flex mt-10 items-center">
+      <div>
+        <div className="form_wrapper flex pb-20 bg-cover bg-center bg-no-repeat justify-end">
+          <div className="p-10 mt-20 mr-56 form-floating xl:w-1/4 rounded dark:bg-slate-800 bg-white  bg-opacity-75">
+            <form className="flex flex-col" onSubmit={this.onSubmit}>
+              <h2 className="text-center font-bold">Car constructor</h2>
+
+              <select
+                defaultValue={''}
+                className="mt-8 rounded py-2 px-3 border-2 required"
+                placeholder="Select year:"
+                onChange={this.onMakeChange}
+                ref={this.carMakeRef}
+                required
+              >
+                <option value="" disabled>
+                  Select make:
+                </option>
+                {[
+                  'Aston Martin',
+                  'Audi',
+                  'Bmw',
+                  'Ferrari',
+                  'Mercedes',
+                  'Porsche',
+                  'Volkswagen',
+                ].map(
+                  (make: string): JSX.Element => (
+                    <option key={make} value={make}>
+                      {make}
+                    </option>
+                  )
+                )}
+              </select>
+
               <input
-                type="radio"
-                id="petrol"
-                name="carFuel"
-                ref={this.carPetrolRef}
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300"
-                defaultChecked={false}
-                onChange={this.onPetrolChange}
+                className="mt-8 border-2 rounded py-2 px-3 required"
+                type="text"
+                placeholder="Car model"
+                name="carModel"
+                ref={this.carModelRef}
+                onChange={this.onModelChange}
+                data-testid="textInput"
+                // id={this.props.subject}
+                required
               />
-              <label htmlFor="petrol" className="ml-2">
-                <span className="block text-m font-medium text-gray-700">Petrol</span>
-              </label>
-              <input
-                type="radio"
-                id="diesel"
-                name="carFuel"
-                ref={this.carDieselRef}
-                className="ml-4 h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300"
-                defaultChecked={false}
-                onChange={this.onDieselChange}
-              />
-              <label htmlFor="diesel" className="ml-2">
-                <span className="block text-m font-medium text-gray-700">Diesel</span>
-              </label>
-              <input
-                type="radio"
-                id="electro"
-                name="carFuel"
-                ref={this.carElectroRef}
-                className="ml-4 h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300"
-                defaultChecked={false}
-                onChange={this.onElectroChange}
-              />
-              <label htmlFor="electro" className="ml-2">
-                <span className="block text-m font-medium text-gray-700">Electro</span>
-              </label>
-            </div>
-            <div className="flex flex-col mt-10">
-              <label htmlFor="DataPicker" className="mb-2 font-medium text-gray-700">
-                Select the year of production:
-              </label>
-              <input
-                type="date"
-                id="DataPicker"
-                name="DataPicker"
-                min="1990-01-01"
-                max="2023-01-01"
-                ref={this.carYearRef}
-                onChange={this.onYearChange}
-                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <div className="flex flex-col mt-10">
-              <label htmlFor="DataPicker" className="mb-2 font-medium text-gray-700">
-                Choose a car photo:
+
+              <div className="flex mt-8 items-center">
                 <input
-                  type="file"
-                  id="PhotoPicker"
-                  name="file"
-                  accept="image/png, image/gif, image/jpeg"
-                  className="block mt-2 w-full text-sm cursor-pointer text-slate-500
+                  type="radio"
+                  id="petrol"
+                  name="carFuel"
+                  ref={this.carPetrolRef}
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300"
+                  defaultChecked={false}
+                  onChange={this.onPetrolChange}
+                  required
+                />
+                <label htmlFor="petrol" className="ml-2">
+                  <span className="block text-m font-medium ">Petrol</span>
+                </label>
+
+                <input
+                  type="radio"
+                  id="diesel"
+                  name="carFuel"
+                  ref={this.carDieselRef}
+                  className="ml-4 h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300"
+                  defaultChecked={false}
+                  onChange={this.onDieselChange}
+                />
+                <label htmlFor="diesel" className="ml-2">
+                  <span className="block text-m font-medium ">Diesel</span>
+                </label>
+
+                <input
+                  type="radio"
+                  id="electro"
+                  name="carFuel"
+                  ref={this.carElectroRef}
+                  className="ml-4 h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300"
+                  defaultChecked={false}
+                  onChange={this.onElectroChange}
+                />
+                <label htmlFor="electro" className="ml-2">
+                  <span className="block text-m font-medium">Electro</span>
+                </label>
+              </div>
+
+              <div className="flex flex-col mt-8">
+                <label htmlFor="DataPicker" className="mb-2 font-medium">
+                  Select the year of production:
+                </label>
+                <input
+                  type="date"
+                  id="DataPicker"
+                  name="DataPicker"
+                  min="1990-01-01"
+                  max="2023-01-01"
+                  ref={this.carYearRef}
+                  onChange={this.onYearChange}
+                  className="w-full px-3 py-2 leading-tight  border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                />
+              </div>
+
+              <div className="flex flex-col mt-8">
+                <label htmlFor="DataPicker" className="mb-2 font-medium ">
+                  Choose a car photo:
+                  <input
+                    type="file"
+                    id="PhotoPicker"
+                    name="file"
+                    accept="image/png, image/gif, image/jpeg"
+                    className="block mt-2 w-full text-sm cursor-pointer
                     file:mr-4 file:py-2 file:px-4
                     file:rounded-full file:border-0
                     file:text-sm file:font-semibold
                   file:bg-violet-50 file:text-violet-700
                   hover:file:bg-violet-100"
-                  ref={this.carPhotoRef}
-                  onChange={this.onPhotoChange}
+                    ref={this.carPhotoRef}
+                    onChange={this.onPhotoChange}
+                    required
+                  />
+                </label>
+              </div>
+
+              <input
+                className="border-2 rounded py-2 px-3 mt-8"
+                type="number"
+                min="0"
+                placeholder="Car mileage"
+                name="carMake"
+                ref={this.carMileageRef}
+                onChange={this.onMileageChange}
+                required
+              />
+
+              <input
+                className="border-2 rounded py-3 px-3 mt-8 text-xl"
+                type="number"
+                min="0"
+                placeholder="Car price $"
+                name="carPrice"
+                ref={this.carPriceRef}
+                onChange={this.onPriceChange}
+                required
+              />
+
+              <div className="flex items-center mt-8">
+                <input
+                  type="checkbox"
+                  id="checkbox"
+                  name="checkbox"
+                  ref={this.checkboxRef}
+                  className="mr-2 h-5 w-5 text-blue-600"
+                  required
                 />
-              </label>
-            </div>
-            {}
-            <button
-              type="submit"
-              className="mt-20 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-            >
-              Submit
-            </button>
-          </form>
+                <label htmlFor="checkbox" className="text-xl">
+                  I consent to my personal data
+                </label>
+              </div>
+
+              <button
+                type="submit"
+                className="mt-8 bg-blue-500 text-white py-3 px-4 rounded hover:bg-blue-700 text-3xl"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
+
+        {this.state.allFormCars.length !== 0 ? (
+          <div className="mt-12 mb-12">
+            <div className="flex justify-center gap-16 flex-wrap">
+              {this.state.allFormCars?.map((car: ICarSchema, index) => (
+                <Card key={index} car={car} />
+              ))}
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
--- react-app/src/models.ts
+++ react-app/src/models.ts
@@ -1,11 +1,9 @@
 export interface ICarSchema {
-  id: number;
   make: string;
   model: string;
   price: number;
   age: number;
   fuel: string;
   miles: string;
-  city: string;
   img: string;
 }
