99f2f9ac1995a70fe9446b3568ead429381266e4
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-04-07 22:18:00 +0200

    feat: add api fetching, loader and error handler

--- react-app/src/components/Cards.tsx
+++ react-app/src/components/Cards.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
-import allCars from '../data/cars';
-import Card from './Card';
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import CharacterCard from './CharacterCard';
 
-import { ICarSchema } from '../models';
+import { ICharacter } from '../models';
+import Loader from './Loader';
 
 const Cards = (): JSX.Element => {
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<true | false>(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get<{ results: ICharacter[] }>(
+          'https://rickandmortyapi.com/api/character'
+        );
+        const data = response.data;
+        setCharacters(data.results);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="mt-12 mb-24">
+      {error && <h1 className="ml-10 text-red-500">{error}</h1>}
+      {isLoading && <Loader />}
       <div className="flex justify-center gap-16 flex-wrap">
-        {allCars.map((car: ICarSchema, index) => (
-          <Card key={index} car={car} />
-        ))}
+        {characters
+          ? characters.map((character: ICharacter) => (
+              <CharacterCard key={character.id} character={character} />
+            ))
+          : null}
       </div>
     </div>
   );
