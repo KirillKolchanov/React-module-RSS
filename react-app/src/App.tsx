dd82d9925736cbb4a7f951d4b780ac8c0ea22c92
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-04-27 13:51:20 +0200

    feat: add Provider to App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,17 +4,21 @@ import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import FormsPage from './pages/FormsPage';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
-function App(): JSX.Element {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/home" element={<Navigate replace to="/" />} />
-      <Route path="about" element={<AboutPage />} />
-      <Route path="forms" element={<FormsPage />} />
-      <Route path="/404" element={<NotFoundPage />} />
-      <Route path="*" element={<Navigate replace to="/404" />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate replace to="/" />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="forms" element={<FormsPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate replace to="/404" />} />
+      </Routes>
+    </Provider>
   );
 }
 
