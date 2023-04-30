c4d70d24014996692e09a5cd9c970d68a698942c
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2023-04-30 21:55:44 +0200

    feat: change Routing in App.tsx

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
@@ -12,11 +12,11 @@ function App() {
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/home" element={<Navigate replace to="/" />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="forms" element={<FormsPage />} />
         <Route path="/404" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate replace to="/404" />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Provider>
   );
